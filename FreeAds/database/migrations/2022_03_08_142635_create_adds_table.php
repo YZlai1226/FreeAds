189f79e501105243f0fb6365fc2b22e5c239bc8b
Karen <yong-zhen.lai@epitech.eu>
2022-03-08 16:37:39 +0100

    product page preparation

---
+++ FreeAds/database/migrations/2022_03_08_142635_create_adds_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adds', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->uniqid('');
+            $table->string('slug')->uniqid('');
+            $table->string('category');
+            $table->text('description');
+            $table->string('picture(s)');
+            $table->integer('price');
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adds');
+    }
+};
