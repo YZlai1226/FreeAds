4a1ed31ab165ef41f0cec941870e081fd2bc85e0
Karen <yong-zhen.lai@epitech.eu>
2022-03-08 14:51:13 +0100

    start over

--- FreeAds/routes/web.php
+++ FreeAds/routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Auth\AuthController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +13,6 @@ use App\Http\Controllers\Auth\AuthController;
 |
 */
 
-Route::get('login', [AuthController::class, 'index'])->name('login');
-
-Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
-
-Route::get('registration', [AuthController::class, 'registration'])->name('register');
-
-Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
-
-Route::get('dashboard', [AuthController::class, 'dashboard']); 
-
-Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/', function () {
+    return view('welcome');
+});
